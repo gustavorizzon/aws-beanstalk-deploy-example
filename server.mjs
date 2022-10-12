19e5503ba48064b0d6850307679fde23a8e8fea8
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2022-10-12 15:45:14 -0300

    feat: project

---
+++ server.mjs
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Hello World' })
+})
+
+app.get('/health', (req, res) => {
+  return res.status(200).send('Healthy')
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`)
+})
